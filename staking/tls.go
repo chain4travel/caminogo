fa8175deeff52dda5a9c862c97d60c47b81ce6ba
cargyriou <charalarg@gmail.com>
2022-09-28 17:57:43 +0300

    Use predefined key pairs in tests instead of hardcoded ones

--- staking/tls.go
+++ staking/tls.go
@@ -26,6 +26,7 @@ import (
 	"math/big"
 	"os"
 	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/chain4travel/caminogo/utils/perms"
@@ -114,17 +115,25 @@ func LoadTLSCertFromBytes(keyBytes, certBytes []byte) (*tls.Certificate, error) 
 }
 
 func LoadTLSCertFromFiles(keyPath, certPath string) (*tls.Certificate, error) {
-	certBytes, err := os.ReadFile(certPath)
+	certBytes, keyBytes, err := ReadRSAKeyPairFiles(keyPath, certPath)
 	if err != nil {
-		return &tls.Certificate{}, err
-	}
-	keyBytes, err := os.ReadFile(keyPath)
-	if err != nil {
-		return &tls.Certificate{}, err
+		return nil, err
 	}
 
 	cert, err := LoadTLSCertFromBytes(keyBytes, certBytes)
 	return cert, err
+}
+
+func ReadRSAKeyPairFiles(keyPath, certPath string) ([]byte, []byte, error) {
+	certBytes, err := os.ReadFile(certPath)
+	if err != nil {
+		return nil, nil, err
+	}
+	keyBytes, err := os.ReadFile(keyPath)
+	if err != nil {
+		return nil, nil, err
+	}
+	return certBytes, keyBytes, nil
 }
 
 func GetRSAKeyPairFromTLSCert(cert *tls.Certificate) (*x509.Certificate, *rsa.PrivateKey, error) {
@@ -188,4 +197,11 @@ func NewCertTemplate() *x509.Certificate {
 		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature | x509.KeyUsageDataEncipherment,
 		BasicConstraintsValid: true,
 	}
+}
+
+func FormatPKCS8PemFile(pemFile string) string {
+	pemOneLine := strings.Replace(pemFile, "\n", "", -1)
+	pemFormatted := pemOneLine[:27] + `\n` + pemOneLine[27:]
+	pemFormatted = pemFormatted[:len(pemFormatted)-25] + `\n` + pemFormatted[len(pemFormatted)-25:]
+	return pemFormatted
 }
