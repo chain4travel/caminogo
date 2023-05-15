3c5576dad3fe593521507cda7d8198b816a77d84
Jan Max Tiedemann <jan.tiedemann@chain4travel.com>
2023-05-15 03:00:39 +0200

    Added KYB Flag

--- vms/platformvm/txs/camino_address_state_tx.go
+++ vms/platformvm/txs/camino_address_state_tx.go
@@ -20,10 +20,12 @@ const (
 	AddressStateRoleBits     = uint64(0b11)
 
 	AddressStateKycVerified    = uint8(32)
-	AddressStateKycVerifiedBit = uint64(0b0100000000000000000000000000000000)
+	AddressStateKycVerifiedBit = uint64(0b00100000000000000000000000000000000)
 	AddressStateKycExpired     = uint8(33)
-	AddressStateKycExpiredBit  = uint64(0b1000000000000000000000000000000000)
-	AddressStateKycBits        = uint64(0b1100000000000000000000000000000000)
+	AddressStateKycExpiredBit  = uint64(0b01000000000000000000000000000000000)
+	AddressStateKybVerified    = uint8(34)
+	AddressStateKybVerifiedBit = uint64(0b10000000000000000000000000000000000)
+	AddressStateKycBits        = uint64(0b11100000000000000000000000000000000)
 
 	AddressStateConsortium      = uint8(38)
 	AddressStateConsortiumBit   = uint64(0b0100000000000000000000000000000000000000)
